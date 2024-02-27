c0206d6e0bedc3bf65056e7ab406574829d9a134
Jeanluca <jeanlucafp@gmail.com>
2024-02-27 15:01:34 -0300

    passando fome com clojure

--- clojure-datomic/project.clj
+++ clojure-datomic/project.clj
@@ -3,5 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [compojure "1.6.1"]
+                 [http-kit "2.3.0"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-json "0.5.0"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [log4j/log4j "1.2.16"]
+                 [org.clojure/data.json "0.2.6"]]
   :repl-options {:init-ns clojure-datomic.core})
--- clojure-datomic/src/clojure_datomic/core.clj
+++ clojure-datomic/src/clojure_datomic/core.clj
@@ -1,6 +1,21 @@
-(ns clojure-datomic.core)
+(ns clojure-datomic.core
+  (:require [compojure.core :refer :all]
+            [org.httpkit.server :as server]
+            [ring.middleware.defaults :refer :all]
+            [ring.util.response :refer [response]]))
+
+(defn hello-handler
+  [req]
+  (response {:status  200
+             :headers {"Content-Type" "application/json"}
+             :body    {:message "Hello, World!"}}))
+
+(defroutes app-routes
+           (GET "/alive" [] hello-handler))
 
 (defn -main
   "Main function"
   [& args]
-  (println "Hello, World!"))
+  (let [port 3000]
+    (server/run-server (wrap-defaults #'app-routes api-defaults) {:port port})
+    (println (str "Running service on port " port))))
